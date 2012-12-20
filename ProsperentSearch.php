88b0256cfffde4433fa93c61b4d071bf0296282a
Prosperent <Prosperent@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-20 00:16:49 +0000

    updated to 2.7
    git-svn-id: https://plugins.svn.wordpress.org/prosperent-powered-product-search/branches/trac1034@642019 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ProsperentSearch.php
+++ ProsperentSearch.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Prosperent Product Search
 Description: Plugin designed to add a product search to a WordPress blog using Prosperent's API.
-Version: 2.6
+Version: 2.7
 Author: Prosperent Brandon
 License: GPL2
 */
